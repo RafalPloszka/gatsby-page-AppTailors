a8ed003258db9f27287456305f391119d127d3fa
Rafal <rafalploszka@live.com>
2021-04-16 17:29:25 +0200

    create Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import * as React from "react";
 import "@fontsource/poppins";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/300-italic.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+
+import Header from "../views/Header";
 
 import '../styles/global.scss';
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Hello, App tailors</h1>
-    </main>      
+    <div id="TOP">
+      <Header />
+    </div>
   )
 }
 
---
+++ src/views/Header/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+// import Image from '../../components/Image';
+// import Button from '../../components/Button';
+
+import * as styles from './Header.module.scss';
+
+
+const Header = props => {
+  return (
+    <header className={styles.header}>
+      <div className={classNames(styles.headerContent, styles.container)}>
+        <div className={styles.imageWrapper}>
+          {/* <Image 
+            // className={styles.}
+            fileName={"graphic-apptailors.jpg"}
+            alt="main graphic"  
+          /> */}
+        </div>
+        <div className={styles.introText}>
+          <h1 className={styles.introTitle}>
+            Your <strong>Tailor Made</strong> Software
+          </h1>
+          <p className={styles.introLead}>
+            Bringing business ideas to life through the use of technology and design.
+          </p>
+          {/* <Button
+            className={styles.button}
+            message={'BROWSE NOW'}
+            onClick={() => console.log('CLICK!')}
+          /> */}
+        </div>
+      </div>
+    </header>
+  )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header
