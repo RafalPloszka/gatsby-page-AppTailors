4f16a049f56d66dac50bf010a423a652f4d30bde
Rafal <rafalploszka@live.com>
2021-04-18 23:13:51 +0200

    add Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,25 @@ import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import "@fontsource/poppins/900.css";
 
+import Navbar from "../views/Navbar";
 import Header from "../views/Header";
 import Services from "../views/Sections/Services";
 import Portfolio from "../views/Sections/Portfolio"
 
 import '../styles/global.scss';
 
+const links = [
+  { to: "/", name: "Services"},
+  { to: "/", name: "Featured projects" },
+  { to: "/", name: "About"},
+  { to: "/", name: "Values" },
+  { to: "/", name: "Blog" },
+]
+
 const IndexPage = () => {
   return (
     <>
+      <Navbar links={links} />
       <div id="TOP">
         <Header />
       </div>
